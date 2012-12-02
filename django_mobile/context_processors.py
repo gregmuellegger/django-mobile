791b7320d13c28a805928433f6fb1539f9aa1710
Matt Robenolt <matt@ydekproductions.com>
2012-12-02 00:13:14 -0800

    Prevent this silly lookup on every request

--- django_mobile/context_processors.py
+++ django_mobile/context_processors.py
@@ -1,6 +1,8 @@
 from django_mobile import get_flavour
 from django_mobile.conf import settings
 
+DEFAULT_MOBILE_FLAVOUR = settings.DEFAULT_MOBILE_FLAVOUR
+
 
 def flavour(request):
     return {
@@ -10,5 +12,5 @@ def flavour(request):
 
 def is_mobile(request):
     return {
-        'is_mobile': get_flavour() == settings.DEFAULT_MOBILE_FLAVOUR,
+        'is_mobile': get_flavour() == DEFAULT_MOBILE_FLAVOUR,
     }
