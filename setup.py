259a6a04643290c879e8cb922a0d234a178c7194
Gregor Müllegger <gregor@muellegger.de>
2014-08-19 22:30:54 +0200

    Adding Python 3.3 classifier to setup.py

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
         'Operating System :: OS Independent',
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
