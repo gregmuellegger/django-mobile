8ffd67c0a128264a9356c53b56b7a265e2953314
Gregor Müllegger <gregor@muellegger.de>
2015-02-06 11:37:24 +0100

    Updating runtests.py script.

--- django_mobile_tests/settings.py
+++ django_mobile_tests/settings.py
@@ -1,15 +1,10 @@
-# Django settings for testsite project.
 import os
+import warnings
+
+
+warnings.simplefilter('always')
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
@@ -18,26 +13,19 @@ DATABASES = {
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
+
+# Set in order to catch timezone aware vs unaware comparisons
+USE_TZ = True
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
+USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'django_mobile_tests', 'media')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -50,7 +38,9 @@ MEDIA_URL = '/media/'
 ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '<REPLACE:SECRET_KEY>'
+SECRET_KEY = '0'
+
+ROOT_URLCONF = 'django_mobile_tests.urls'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -69,10 +59,8 @@ MIDDLEWARE_CLASSES = (
     'django_mobile.middleware.SetFlavourMiddleware',
 )
 
-ROOT_URLCONF = 'django_mobile_tests.urls'
-
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'django_mobile_tests', 'templates'),
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -95,8 +83,9 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django_mobile.context_processors.is_mobile",
 )
 
-try:
-    from local_settings import *
-except ImportError:
-    pass
 
+import django
+if django.VERSION < (1, 6):
+    TEST_RUNNER = 'discover_runner.DiscoverRunner'
+else:
+    TEST_RUNNER = 'django.test.runner.DiscoverRunner'
---
+++ runtests.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import argparse
+import os, sys
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django_mobile_tests.settings'
+
+
+# Adding current directory to ``sys.path``.
+parent = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, parent)
+
+
+def runtests(*argv):
+    argv = list(argv) or [
+        'django_mobile',
+        'django_mobile_tests',
+    ]
+    opts = argparser.parse_args(argv)
+
+    if opts.coverage:
+        from coverage import coverage
+        test_coverage = coverage(
+            branch=True,
+            source=['django_mobile'])
+        test_coverage.start()
+
+    # Run tests.
+    from django.core.management import execute_from_command_line
+    execute_from_command_line([sys.argv[0], 'test'] + opts.appname)
+
+    if opts.coverage:
+        test_coverage.stop()
+
+        # Report coverage to commandline.
+        test_coverage.report(file=sys.stdout)
+
+
+argparser = argparse.ArgumentParser(description='Process some integers.')
+argparser.add_argument('appname', nargs='*')
+argparser.add_argument('--no-coverage', dest='coverage', action='store_const',
+    const=False, default=True, help='Do not collect coverage data.')
+
+
+if __name__ == '__main__':
+    runtests(*sys.argv[1:])
