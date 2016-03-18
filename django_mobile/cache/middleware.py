bfe25be8567b349e8ec335cc0d1dcf29f65fcc21
Юрий Пайков <cry5tal@cry5tal.in>
2016-03-18 16:00:17 +0500

    Fixed bug in `cache_page` decorator
    It is necessary to introduce `HTTP_X_FLAVOUR` header before standard Django `CacheMiddleware` in request
    and patch `Vary:` headers before standard Django `CacheMiddleware` in response as well
    
    Thus, nesting flavour decorator inside cache decorator is not correct and we need two separate flavour decorators for caching with flavour

--- django_mobile/cache/middleware.py
+++ django_mobile/cache/middleware.py
@@ -2,10 +2,11 @@ from django_mobile import get_flavour, _set_request_header
 from django.utils.cache import patch_vary_headers
 
 
-class CacheFlavourMiddleware(object):
+class FetchFromCacheFlavourMiddleware(object):
     def process_request(self, request):
         _set_request_header(request, get_flavour(request))
 
+class UpdateCacheFlavourMiddleware(object):
     def process_response(self, request, response):
         patch_vary_headers(response, ['X-Flavour'])
         return response
