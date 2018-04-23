f59ee738d83dfe1ef4d009b1905248b494d6c7c8
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-04-23 10:34:02 +0100

    Fixup URL’s.

--- django_mobile_tests/urls.py
+++ django_mobile_tests/urls.py
@@ -1,7 +1,4 @@
-try:
-    from django.conf.urls.defaults import *
-except ImportError:
-    from django.conf.urls import *
+from django.conf.urls import url
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django_mobile.cache import cache_page
@@ -12,7 +9,7 @@ def index(request):
     }, context_instance=RequestContext(request))
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', index),
     url(r'^cached/$', cache_page(60*10)(index)),
-)
+]
